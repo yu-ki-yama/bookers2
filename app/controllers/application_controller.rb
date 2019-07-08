ab60d80bbcfb09cdc621f0d4ed8d3c5d5caf98cd
yu-ki-yama <framewebwork212@gmail.com>
2019-07-08 21:15:45 +0900

    [Update] deviseの動作確認終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # リダイレクト先を初期設定から変更
+  class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+
+    end
+  end
+
+  
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
